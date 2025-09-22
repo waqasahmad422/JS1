dffd1233c574e097d4a6e0a79694cd5e9aebbfff
waqasahmad422 <waqasahmadshah@545818gmail.com>
2025-09-22 09:00:53 -0700

    8th task uploaded

--- Task8/script.js
+++ Task8/script.js
@@ -1,1 +1,20 @@
-alert("Hello, World!");
+let expression=(((15 * 3 * 2) / 3 + (4*4)) * 2 * (8 / 2)**2);
+
+alert("Answer of this expressiong is: "+ expression);
+
+// solution:
+// (((15 x 3 x 2) ÷  3 + (4x4)) x 2 x (8 ÷  2)²)
+
+// (((15 x 6) ÷ 3 + (4 x 4)) x 2 x (4)²)
+
+// ((90 ÷  3+ (4 x4)) x 2 x (4)²)
+
+// ((90 ÷  3 + 16) x 2 x (4)²)
+
+// ((30 + 16) x 2 x 16)
+
+// 46 x 2 x 16
+
+// 46 x 32
+
+// awnser is:  1472
