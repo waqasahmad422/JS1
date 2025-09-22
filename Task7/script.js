4a7081f272509ac4b2fc3b2b5c46b5d35653928c
waqasahmad422 <waqasahmadshah@545818gmail.com>
2025-09-22 08:40:21 -0700

    7th task uploaded

---
+++ Task7/script.js
@@ -0,0 +1,15 @@
+let count= 6;
+
+// first value (6) display then increment value (7)
+alert("post increment " + (count++)); 
+
+// first value increment (7) then display (7)
+alert("pre increment " + (++count));
+alert(count);
+
+// first value (6) display then decrement value (5)
+alert("post decrement " + (count--));
+
+// first value increment (5) then display (5)
+alert("pre decrement " + (--count));
+alert(count);
