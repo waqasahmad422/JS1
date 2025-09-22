41dda8871b24582423ffb8ba737c4f330bd6c196
waqasahmad422 <waqasahmadshah@545818gmail.com>
2025-09-22 07:59:57 -0700

    3rd Task uploaded

---
+++ Task3/script.js
@@ -0,0 +1,10 @@
+// first two number create 
+let num1=10;
+let num2=5;
+
+
+alert("Sum is: "+(num1 + num2));
+alert("Substraction of two number is: "+(num1 - num2));
+alert("Product of two number is: "+(num1 * num2));
+alert("Division of two number is: "+(num1 / num2));
+alert("Module of two number is: "+(num1 % num2));
