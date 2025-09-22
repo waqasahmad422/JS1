13c94fba7c321bbf0bc7d230892f0c6c73bb2f0e
waqasahmad422 <waqasahmadshah@545818gmail.com>
2025-09-22 08:32:33 -0700

    6th task uploade

---
+++ Task6/script.js
@@ -0,0 +1,6 @@
+let celsius= +prompt("Enter temprature value in Celsius");
+
+// Formula for conversion (celsius * (5/9)) + 32
+let fahrenheit= (celsius * (9/5)) + 32;
+
+alert("Fahrenheit is: "+ fahrenheit);
